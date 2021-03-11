33274e2a7ab9a20fba6977acd26f9319241ee64c
Dongxiao <kuit@msn.com>
2021-03-11 17:34:43 +0800

    #nd05g1 add cordova intent test

--- Cordova/Test01/src/app/home/home.page.ts
+++ Cordova/Test01/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+declare let intentPlugin: IntentPlugin.Intent;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +8,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() {
+    intentPlugin.addIntentListener((ret) => {
+      this.onIntentReceived(ret);
+    });
+  }
+
+  async onIntentReceived(intent: IntentPlugin.ReceivedIntent) {
+    console.log('Received intent', intent);
+    try {
+      console.log("sendIntentResponse");
+      await intentPlugin.sendIntentResponse({b:"2"}, intent.intentId);
+    }
+    catch (e) {
+        console.error("sendIntentResponse error:", e);
+        throw e;
+    }
+  }
+
+  public async testIntent() {
+    console.log("test Intent");
+
+    try {
+      let ret = await intentPlugin.sendIntent("https://scanner.elastos.net/scanqrcode", {a: 1});
+      console.log('intent response:', ret);
+      console.log("sendUrlIntent");
+      await intentPlugin.sendUrlIntent("https://did.elastos.net/credaccess/eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkaWQ6ZWxhc3RvczppcVduUFJLWTltTktvUmRETXJrSGRlRHBRQmk0WnRvekQ0IiwiY2FsbGJhY2t1cmwiOiJodHRwczovL2FwaS5jeWJlcnJlcHVibGljLm9yZy9hcGkvdXNlci9sb2dpbi1jYWxsYmFjay1lbGEiLCJub25jZSI6IjZiNzkyZDVhLTBlYTItNDU4MC1hYWY2LWRiM2FjZjA2OGZiMiIsImNsYWltcyI6e30sIndlYnNpdGUiOnsiZG9tYWluIjoiaHR0cHM6Ly93d3cuY3liZXJyZXB1YmxpYy5vcmciLCJsb2dvIjoiaHR0cHM6Ly93d3cuY3liZXJyZXB1YmxpYy5vcmcvYXNzZXRzL2ltYWdlcy9jcl9lbGFfd2FsbGV0LnN2ZyJ9LCJpYXQiOjE2MTUxODYyNzUsImV4cCI6MTYxNTc5MTA3NX0._ppCH7YHd5SVoZHZI2YeYww6OlAjf5GIG3QqqSXf5rMw8oBmvdF7Nld_WMvnVQUvSnmnadRPZsYW66-Zi7T1Fg ");
+    }
+    catch (e) {
+      console.error("sendIntent error:", e);
+      throw e;
+    }
+
+    console.log("tested Intent");
+  }
 
 }
