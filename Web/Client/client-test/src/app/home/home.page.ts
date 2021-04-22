15bce9e5d32a18477e822a84770fbd293e9dd231
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2021-04-22 16:12:32 +0800

    CU-tfbbjm Added walletconnect entry points on the web client test

--- Web/Client/client-test/src/app/home/home.page.ts
+++ Web/Client/client-test/src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Hive , connectivity, DID, Wallet, localization, theme } from "@elastosfoundation/elastos-connectivity-sdk-cordova";
 import { EssentialsConnector, LinkType } from "@elastosfoundation/essentials-connector-client-browser";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import Web3, {  } from "web3";
 
 @Component({
   selector: 'app-home',
@@ -80,6 +82,139 @@ export class HomePage {
     console.log("Trying to get credentials");
     let response = await wallet.pay({receiver: '0x0aD689150EB4a3C541B7a37E6c69c1510BCB27A4', amount: 0.01, memo: 'just test memo', currency: 'ELA/ETHSC'});
     console.log("pay respone", response);
+  }
+
+  private getWalletConnectProvider(): WalletConnectProvider {
+    //  Create WalletConnect Provider
+    const provider = new WalletConnectProvider({
+      rpc: {
+        20: "https://testnet.elastos.io/eth",
+        21: "https://api-testnet.elastos.io/eth",
+      },
+      //bridge: "http://192.168.1.3/"
+      bridge: "http://192.168.1.3:5001"
+    });
+    return provider;
+  }
+
+  // https://docs.walletconnect.org/quick-start/dapps/web3-provider
+  public async testWalletConnectConnect() {
+    let provider = this.getWalletConnectProvider();
+
+    console.log("Connected?", provider.connected);
+
+    // Subscribe to accounts change
+    provider.on("accountsChanged", (accounts: string[]) => {
+      console.log(accounts);
+    });
+
+    // Subscribe to chainId change
+    provider.on("chainChanged", (chainId: number) => {
+      console.log(chainId);
+    });
+
+    // Subscribe to session disconnection
+    provider.on("disconnect", (code: number, reason: string) => {
+      console.log(code, reason);
+    });
+
+    // Subscribe to session disconnection
+    provider.on("error", (code: number, reason: string) => {
+      console.error(code, reason);
+    });
+
+    //  Enable session (triggers QR Code modal)
+    console.log("Connecting to wallet connect");
+    let enabled = await provider.enable();
+    console.log("CONNECTED to wallet connect", enabled);
+
+    const web3 = new Web3(provider as any); // HACK
+
+    //  Get Chain Id
+    const chainId = await web3.eth.getChainId();
+    console.log("Chain ID: ", chainId);
+
+    if (chainId != 20 && chainId != 21) {
+      console.error("ERROR: Connected to wrong ethereum network "+chainId+". Not an elastos network. Check that the wallet app is using an Elastos network.");
+      return;
+    }
+
+    /*
+    interface RequestArguments {
+      method: string;
+      params?: unknown[] | object;
+    }
+
+    // Send JSON RPC requests
+    const result = await provider.request(payload: RequestArguments);
+
+    // Close provider session
+    await provider.disconnect()
+    */
+
+    /*
+
+    //  Get Accounts
+    const accounts = await web3.eth.getAccounts();
+
+    //  Get Chain Id
+    const chainId = await web3.eth.chainId();
+
+    //  Get Network Id
+    const networkId = await web3.eth.net.getId();
+
+    // Send Transaction
+    const txHash = await web3.eth.sendTransaction(tx);
+
+    // Sign Transaction
+    const signedTx = await web3.eth.signTransaction(tx);
+
+    // Sign Message
+    const signedMessage = await web3.eth.sign(msg);
+
+    // Sign Typed Data
+    const signedTypedData = await web3.eth.signTypedData(msg);
+    */
+
+    const accounts = await web3.eth.getAccounts();
+
+    let contractAbi = require("../../assets/erc721.abi.json");
+    let contractAddress = "0x5b462bac2d07223711aA0e911c846e5e0E787654"; // Elastos Testnet
+    let contract = new web3.eth.Contract(contractAbi, contractAddress);
+
+    let gasPrice = await web3.eth.getGasPrice();
+    console.log("Gas price:", gasPrice);
+
+    console.log("Sending transaction with account address:", accounts[0]);
+    let transactionParams = {
+        from: accounts[0],
+        gasPrice: gasPrice,
+        gas: 5000000,
+        value: 0
+    };
+
+    let address = accounts[0];
+    let tokenId = Math.floor(Math.random()*10000000000);
+    let tokenUri = "https://my.token.uri.com";
+    console.log("Calling smart contract through wallet connect", address, tokenId, tokenUri);
+    contract.methods.mint(address, tokenId, tokenUri).send(transactionParams)
+        .on('transactionHash', (hash) => {
+          console.log("transactionHash", hash);
+        })
+        .on('receipt', (receipt) => {
+          console.log("receipt", receipt);
+        })
+        .on('confirmation', (confirmationNumber, receipt) => {
+          console.log("confirmation", confirmationNumber, receipt);
+        })
+        .on('error', (error, receipt) => {
+          console.error("error", error);
+        });
+  }
+
+  public async testWalletConnectDisconnect() {
+    let provider = this.getWalletConnectProvider();
+    provider.disconnect();
   }
 
   /*public async testHiveAuth() {
