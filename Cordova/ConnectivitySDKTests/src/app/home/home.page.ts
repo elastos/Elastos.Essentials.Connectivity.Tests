11f4ef6acd8654061d99d8fd74f01bea33c2f9f4
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2021-04-28 15:03:12 +0800

    Fixed build error, duplicate Platform

--- Cordova/ConnectivitySDKTests/src/app/home/home.page.ts
+++ Cordova/ConnectivitySDKTests/src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
 import { Hive , connectivity, DID, Wallet, localization, theme } from "@elastosfoundation/elastos-connectivity-sdk-cordova";
 import { EssentialsConnector } from "@elastosfoundation/essentials-connector-cordova";
 import { LocalIdentityConnector, localIdentity } from "@elastosfoundation/elastos-connector-localidentity-cordova";
